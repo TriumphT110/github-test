faa58d14bb70f19f7f8cde4c42eb618c94c444e6
Brooks <brooks@brookss-macbook-pro.local>
2018-03-28 09:44:17 +0100

    Initial Commit

---
+++ github test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github test
+//
+//  Created by Brooks on 28/03/2018.
+//  Copyright © 2018 Brooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
